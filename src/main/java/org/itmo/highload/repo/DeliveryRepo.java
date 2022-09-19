ab8b3dd53c130310cd03510d21c7f669394f2f24
Maria Karaseva <m.karaseva2000@mail.ru>
2022-09-19 16:35:55 +0300

    feat(repository): add Delivery repository

--- src/main/java/org/itmo/highload/repo/DeliveryRepo.java
+++ src/main/java/org/itmo/highload/repo/DeliveryRepo.java
@@ -1,12 +1,11 @@
 package org.itmo.highload.repo;
 
-import org.itmo.highload.model.UserData;
+import org.itmo.highload.model.Delivery;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-
 @Repository
-public interface UserDataRepo extends CrudRepository<UserData, UUID> {
+public interface DeliveryRepo extends CrudRepository<Delivery, UUID> {
 }
--- src/main/java/org/itmo/highload/repo/UserRepo.java
+++ src/main/java/org/itmo/highload/repo/UserRepo.java
@@ -2,8 +2,10 @@ package org.itmo.highload.repo;
 
 import org.itmo.highload.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepo extends CrudRepository<User, UUID> {
 }
