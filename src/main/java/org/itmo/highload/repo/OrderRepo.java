867d13f96c6c5ccaa2ce2ecb8bc0d9540c5b03fe
Maria Karaseva <m.karaseva2000@mail.ru>
2022-09-20 00:49:59 +0300

    fixed(repo): delete old repo

--- src/main/java/org/itmo/highload/repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package org.itmo.highload.repo;
-
-import org.itmo.highload.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepo extends JpaRepository<Order, UUID> {
-}
