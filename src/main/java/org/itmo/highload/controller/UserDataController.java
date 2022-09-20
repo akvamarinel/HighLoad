c24016aaa85a482aaab90084d13ae459be873576
arsnazarenko <arsnazarenko@gmail.com>
2022-09-21 02:51:08 +0300

    dto: create user dto

---
+++ src/main/java/org/itmo/highload/controller/UserDataController.java
@@ -0,0 +1,4 @@
+package org.itmo.highload.controller;
+
+public class UserDataController {
+}
--- src/main/java/org/itmo/highload/dto/UserDataDto.java
+++ src/main/java/org/itmo/highload/dto/UserDataDto.java
@@ -1,23 +1,40 @@
 package org.itmo.highload.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.itmo.highload.model.UserRole;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDataDto {
+
+    @NotNull
     UUID id;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     String name;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
     String surname;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    String login;
+
+    @NotNull
     UserRole role;
-
-    public UserDataDto(UUID id, String name, String surname, UserRole role) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.role = role;
-    }
-
-    public UserDataDto() {
-    }
 }
