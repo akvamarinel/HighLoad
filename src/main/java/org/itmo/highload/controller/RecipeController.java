e72f5f9f73fc1c081791714cc89ac7c7daac059e
arsnazarenko <arsnazarenko@gmail.com>
2022-10-03 02:56:07 +0300

    feat: recipe repo and controller

---
+++ src/main/java/org/itmo/highload/controller/RecipeController.java
@@ -0,0 +1,45 @@
+package org.itmo.highload.controller;
+
+import lombok.AllArgsConstructor;
+import org.itmo.highload.model.Recipe;
+import org.itmo.highload.repo.RecipeRepo;
+import org.itmo.highload.service.RecipeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+public class RecipeController {
+
+
+    private final RecipeService recipeService;
+
+    @GetMapping("/recipes/{id}")
+    ResponseEntity<Recipe> getOne(@PathVariable UUID id) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/recipes")
+    ResponseEntity<List<Recipe>> getAll() {
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/recipes")
+    ResponseEntity<Recipe> create(@RequestBody Recipe recipe) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/recipes/{id}")
+    ResponseEntity<Recipe> update(@PathVariable UUID id, @RequestBody Recipe recipe) {
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/recipes/{id}")
+    ResponseEntity<Recipe> deleteById(@PathVariable UUID id) {
+        return ResponseEntity.ok(null);
+    }
+
+}
---
+++ src/main/java/org/itmo/highload/service/RecipeService.java
@@ -0,0 +1,32 @@
+package org.itmo.highload.service;
+
+import lombok.AllArgsConstructor;
+import org.itmo.highload.model.Recipe;
+import org.itmo.highload.repo.RecipeRepo;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class RecipeService {
+
+    private final RecipeRepo recipeRepo;
+
+
+    Optional<Recipe> getOne(UUID id) {
+        return recipeRepo.findById(id);
+    }
+
+    Iterable<Recipe> getAll() {
+        return recipeRepo.findAll();
+    }
+
+    Recipe create(Recipe recipe) {
+        return recipeRepo.save(recipe);
+    }
+
+//    Recipe update(UUID id, Recipe recipe) {
+//        recipeRepo.findById(id).map();
+//    }
+
+}
