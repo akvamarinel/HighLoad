94a5db3381f1baa795e507e66b53db8db74fe55c
arsnazarenko <arsnazarenko@gmail.com>
2022-10-03 03:14:01 +0300

    fix: user data controller

--- src/main/java/org/itmo/highload/controller/UserDataController.java
+++ src/main/java/org/itmo/highload/controller/UserDataController.java
@@ -1,14 +1,15 @@
 package org.itmo.highload.controller;
 
+import lombok.AllArgsConstructor;
 import org.itmo.highload.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
+@RequestMapping("user-data")
 public class UserDataController {
     @Autowired
-    UserDataService userDataService;
-
-
-
+    private UserDataService userDataService;
 }
