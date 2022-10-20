7133198a08b8e4fb7c0b31dee05fbffeb00a4cc3
arsnazarenko <arsnazarenko@gmail.com>
2022-10-20 03:50:44 +0300

    merge

--- src/main/java/org/itmo/highload/dish/service/DishService.java
+++ src/main/java/org/itmo/highload/dish/service/DishService.java
@@ -26,11 +26,8 @@ public class DishService {
     private final RecipeMapper recipeMapper;
     private final DishMapper dishMapper;
     private final RecipeRepo recipeRepo;
-<<<<<<< HEAD
     private final RestaurantRepo restaurantRepo;
-=======
 
->>>>>>> f89148a3ed84a337c4864a5287d0afd1936fef1a
 
     public Dish getOne(UUID id) {
         return dishRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(Dish.class, id));
@@ -47,33 +44,5 @@ public class DishService {
         Recipe recipe = recipeMapper.toModel(recipeDto);
         dish.setRecipe(recipeRepo.save(recipe));
         return dishRepo.save(dish);
-
-//        Dish dish = dishMapper.toModel(dishDto);
-//        RecipeDto recipeDto = dishDto.getRecipe();
-//        Recipe recipe = recipeMapper.toModel(recipeDto);
-//        List<FoodInRecipe> foodInRecipeList = recipeDto.getFoodInRecipe().stream()
-//                .map(e -> {
-//                    Foodstuff food = foodstuffRepo.findById(e.getFoodstuffId())
-//                            .orElseThrow(() -> new EntityNotFoundException(Foodstuff.class, e.getFoodstuffId()));
-//                    return FoodInRecipe.builder()
-//                            .id(new FoodInRecipeKey(food.getId(), recipe.getId()))
-//                            .foodstuff(food)
-//                            .recipe(recipe)
-//                            .weight(e.getWeight())
-//                            .build();
-//                }).collect(Collectors.toList());
-//        recipe.setFoodInRecipe(foodInRecipeList);
-//        recipeRepo.save(recipe);
-//        foodInRecipeList.forEach(foodInRecipeRepo::save);
-//        foodInRecipeList.forEach(fr -> {
-//            Foodstuff foodstuff = fr.getFoodstuff();
-//            foodstuff.getFoodInRecipe().add(fr);
-//            foodstuffRepo.save(foodstuff);
-//        });
-//
-//        dish.setRecipe(recipe);
-//        return dishRepo.save(dish);
-//    }
-
     }
 }
